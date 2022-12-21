d7e06a89888f273c4106328ad2c3f9bdf09a075b
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2022-12-21 04:21:09 +0100

    userController

---
+++ config/allowedOrigins.js
@@ -0,0 +1,10 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.danrepairshop.com',
+    'https://danrepairshop.com',
+    'https://rs-family.netlify.app',
+    'https://www.rs-family.netlify.app',
+
+];
+
+module.exports = allowedOrigins;
